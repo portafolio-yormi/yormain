562c8509f4f474b1421b306794b26fedff0861ce
Yormi Altamiranda <yormian@gmail.com>
2024-03-27 13:47:26 -0500

    :sparkles: Upload

---
+++ src/data/info.ts
@@ -0,0 +1,157 @@
+export const info = {
+  baseUrl: "https://astro-portfolio-uzair.vercel.app",
+  name: "Yormi Altamiranda",
+  jobDescription: "Web Developer",
+  about: `Soy ingeniero en Informática y vivo en Bogotá, Colombia. Me gusta crear cosas que viven en Internet, ya sean sitios web, aplicaciones o cualquier otra cosa. He estado trabajando durante 10 años, con una historia demostrada de trabajo en la industria del desarrollo web. en Desarrollo Front-end. Tengo experiencia en HTML, CSS, SCSS, JavaScript, Bootstrap, tailwind CSS, Wordpress, WooCommerce y Elementor. Por lo tanto, he completado con éxito varios proyectos web desde cero. Sin embargo, aprendo continuamente y nunca dejo de explorar nuevas tecnologías. Así, también he adquirido conocimientos de Node JS, Express JS, SupaBase, firebase y Flutter con el tiempo.
+  `,
+
+  experience: [
+    {
+      name: "ATH (A Todas Horas S.A.)",
+      location: "BOG - CO",
+      startDate: "Mar 2021",
+      endDate: "Presente",
+      description: [
+        "- Líder de Equipo.",
+        "- UX/UI Designer.",
+        "- Frontend Developer.",
+      ],
+    },
+
+    {
+      name: "Parchita Creative",
+      location: "BOG - CO",
+      startDate: "Ene 2021",
+      endDate: "Mar 2021",
+      description: [
+        "- Prototipos UX/UI Figma - Adobe XD.",
+        "- Mantención Mailing.",
+        "- Diseño y Desarrollo eCommerce.",
+      ],
+    },
+
+    {
+      name: "BRM ",
+      location: "BOG - CO",
+      startDate: "Sep 2020",
+      endDate: "Dic 2020",
+      description: [
+        "- UX/UI Designer.",
+        "- Frontend Developer.",
+      ],
+    },
+
+    {
+      name: "QUBIT MARKETING",
+      location: "BOG - CO",
+      startDate: "Sep 2018",
+      endDate: "Sep 2020",
+      description: [
+        "- Prototipos UX/UI Figma - Adobe XD.",
+        "- Mantención Mailing.",
+        "- Diseño y Desarrollo eCommerce.",
+      ],
+    },
+    {
+      name: "CLICNOVO",
+      location: "VAL - VEN",
+      startDate: "May 2017",
+      endDate: "Sep 2018",
+      description: [
+        "- Diseñador Web",
+        "- Mantención Mailing.",
+        "- Diseño y Desarrollo eCommerce.",
+      ],
+    },
+  ],
+
+  education: [
+    {
+      name: "Instituto Universitario de Tecnología Valencia (IUTVAL)",
+      location: "Valencia, Edo. Carabobo, Venezuela",
+      startDate: "2010",
+      endDate: "2017",
+      description: [
+        "Ingeniero en Informática",
+      ],
+    },
+  ],
+
+  socialMedia: {
+    // facebook: "https://www.facebook.com/profile.php?id=100022899849583",
+    // twitter: "https://twitter.com/Uzzii_21",
+    github: "https://github.com/yormi-altamiranda",
+    email: "mailto:yormain@gmail.com",
+    linkedin: "https://www.linkedin.com/in/yormi-altamiranda-b7352111b/",
+  },
+
+  projects: [
+    {
+      title: "Terracol",
+      isFeatured: true,
+      thumbnail: "/assets/images/thumbnail-projects-1.jpg",
+      // githubUrl: "https://github.com/uzzii-21/astro-portfolio",
+      iconWp: true,
+      iconHtml: false,
+      iconSass:  false,
+      iconCss: false,
+      liveUrl: "https://terracol.com/",
+    },
+    {
+      title: "Arrazola Art",
+      isFeatured: true,
+      thumbnail: "/assets/images/thumbnail-projects-2.jpg",
+      // githubUrl: "https://github.com/uzzii-21/astro-portfolio",
+      iconWp: true,
+      iconHtml: false,
+      iconSass:  false,
+      iconCss: false,
+      liveUrl: "https://arrazolaart.com/",
+    },
+    {
+      title: "Zoe Pure Pets",
+      isFeatured: true,
+      thumbnail: "/assets/images/thumbnail-projects-3.jpg",
+      // githubUrl: "https://github.com/uzzii-21/astro-portfolio",
+      iconWp: true,
+      iconHtml: false,
+      iconSass:  false,
+      iconCss: false,
+      liveUrl: "https://zoepurepets.com/",
+    },
+    {
+      title: "Odysea Flowers",
+      isFeatured: true,
+      thumbnail: "/assets/images/thumbnail-projects-4.jpg",
+      // githubUrl: "https://github.com/uzzii-21/astro-portfolio",
+      iconWp: true,
+      iconHtml: false,
+      iconSass:  false,
+      iconCss: false,
+      liveUrl: "https://odyseaflowers.com/",
+    },
+    {
+      title: "Nail Fever the Falls",
+      isFeatured: true,
+      thumbnail: "/assets/images/thumbnail-projects-5.jpg",
+      // githubUrl: "https://github.com/uzzii-21/astro-portfolio",
+      iconWp: true,
+      iconHtml: false,
+      iconSass:  false,
+      iconCss: false,
+      liveUrl: "https://nailfeverthefalls.com/",
+    },
+
+    // {
+    //   title: "Plantilla Mailing",
+    //   isFeatured: true,
+    //   thumbnail: "/assets/images/astro-portfolio.png",
+    //   githubUrl: "https://github.com/uzzii-21/astro-portfolio",
+    //   iconWp: false,
+    //   iconHtml: true,
+    //   iconSass:  false,
+    //   iconCss: true,
+    //   liveUrl: "https://astro-portfolio-uzair.vercel.app/",
+    // },
+  ],
+};
